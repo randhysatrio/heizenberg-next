1914d7855684c74444dde83db8ee671eb85c4c04
randhysatrio <randhysatrio@gmail.com>
2024-02-13 22:46:14 +0700

    update the janky footer

--- src/app/_components/Footer/index.tsx
+++ src/app/_components/Footer/index.tsx
@@ -1,66 +1,69 @@
+import ClickableText from '../UI/ClickableText';
+
 import { FaFacebookSquare, FaInstagram } from 'react-icons/fa';
 import { FaComputer, FaXTwitter } from 'react-icons/fa6';
-import ClickableText from '../UI/ClickableText';
 
 export default function Footer() {
   return (
-    <footer className="flex w-full justify-between gap-6 border-t px-5 py-8 sm:px-20">
+    <footer className="flex w-full justify-between gap-6 border-t px-5 py-8 sm:px-20 lg:px-80 lg:py-12">
       <section className="grid grid-cols-2 gap-6 sm:gap-20">
-        <div className="col-span-1 flex flex-col gap-4">
-          <h3 className="font-rubik-bold text-xl sm:text-2xl">HZN</h3>
+        <div className="col-span-1 flex flex-col gap-4 lg:gap-6">
+          <h3 className="font-rubik-bold text-xl sm:text-2xl lg:text-3xl">
+            HZN
+          </h3>
 
-          <section className="flex w-full flex-col gap-1">
+          <section className="flex w-full flex-col gap-1 lg:gap-3">
             <ClickableText
               whileTap={{ scale: 0.95 }}
-              className="cursor-pointer text-black/75 hover:text-sky-400 sm:text-lg"
+              className="cursor-pointer text-black/75 hover:text-sky-400 sm:text-lg lg:text-xl"
             >
               About Us
             </ClickableText>
             <ClickableText
               whileTap={{ scale: 0.95 }}
-              className="cursor-pointer text-black/75 hover:text-sky-400 sm:text-lg"
+              className="cursor-pointer text-black/75 hover:text-sky-400 sm:text-lg lg:text-xl"
             >
               Blog
             </ClickableText>
             <ClickableText
               whileTap={{ scale: 0.95 }}
-              className="cursor-pointer text-black/75 hover:text-sky-400 sm:text-lg"
+              className="cursor-pointer text-black/75 hover:text-sky-400 sm:text-lg lg:text-xl"
             >
               Careers
             </ClickableText>
             <ClickableText
               whileTap={{ scale: 0.95 }}
-              className="cursor-pointer text-black/75 hover:text-sky-400 sm:text-lg"
+              className="cursor-pointer text-black/75 hover:text-sky-400 sm:text-lg lg:text-xl"
             >
               Policy
             </ClickableText>
             <ClickableText
               whileTap={{ scale: 0.95 }}
-              className="cursor-pointer text-black/75 hover:text-sky-400 sm:text-lg"
+              className="cursor-pointer text-black/75 hover:text-sky-400 sm:text-lg lg:text-xl"
             >
               Sell Product
             </ClickableText>
           </section>
         </div>
-        <div className="col-span-1 flex flex-col gap-4">
+        <div className="col-span-1 flex flex-col gap-4 lg:gap-6">
           <h3 className="font-rubik-bold text-xl sm:text-2xl">HELP</h3>
 
-          <section className="flex w-full flex-col gap-2">
+          <section className="flex w-full flex-col gap-1 lg:gap-3">
             <ClickableText
               whileTap={{ scale: 0.95 }}
-              className="cursor-pointer text-black/75 hover:text-sky-400 sm:text-lg"
+              className="cursor-pointer text-black/75 hover:text-sky-400 sm:text-lg lg:text-xl"
             >
               Help Center
             </ClickableText>
             <ClickableText
               whileTap={{ scale: 0.95 }}
-              className="cursor-pointer text-black/75 hover:text-sky-400 sm:text-lg"
+              className="cursor-pointer text-black/75 hover:text-sky-400 sm:text-lg lg:text-xl"
             >
               Track Order
             </ClickableText>
             <ClickableText
               whileTap={{ scale: 0.95 }}
-              className="cursor-pointer text-black/75 hover:text-sky-400 sm:text-lg"
+              className="cursor-pointer text-black/75 hover:text-sky-400 sm:text-lg lg:text-xl"
             >
               Payment
             </ClickableText>
@@ -74,9 +77,9 @@ export default function Footer() {
           <h2 className="font-rubik-bold text-2xl sm:text-4xl">HZN</h2>
         </div>
         <div className="flex items-center gap-4 sm:gap-6">
-          <FaInstagram className="h-6 w-6 text-black/75 hover:text-sky-400 sm:h-8 sm:w-8" />
-          <FaXTwitter className="h-6 w-6 sm:h-8 sm:w-8" />
-          <FaFacebookSquare className="h-6 w-6 sm:h-8 sm:w-8" />
+          <FaInstagram className="h-6 w-6 cursor-pointer text-black/75 hover:text-sky-400 sm:h-8 sm:w-8" />
+          <FaXTwitter className="h-6 w-6 cursor-pointer text-black/75 hover:text-sky-400 sm:h-8 sm:w-8" />
+          <FaFacebookSquare className="h-6 w-6 cursor-pointer text-black/75 hover:text-sky-400 sm:h-8 sm:w-8" />
         </div>
       </section>
     </footer>
